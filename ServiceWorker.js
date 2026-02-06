cf803f077d039aa2216064684d7f28878c38786a
Saiful Islam <saifulanik@icloud.com>
2026-02-06 19:39:10 +0600

    Initial commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-SpearMania-1.4";
+const contentToCache = [
+    "Build/SpearMania-Airdrop.loader.js",
+    "Build/SpearMania-Airdrop.framework.js",
+    "Build/SpearMania-Airdrop.data",
+    "Build/SpearMania-Airdrop.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
