c9066bb4e81e3f2ee61ace7b13abbd6231fd86f4
Saiful Islam <saifulanik@icloud.com>
2026-03-26 02:03:38 +0600

    scene fix

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-Degen Island-2.3";
+const cacheName = "DefaultCompany-Degen Island-2.4";
 const contentToCache = [
     "Build/SpearMania-Airdrop.loader.js",
     "Build/SpearMania-Airdrop.framework.js",
