ca6f2f94c5cbccf8a2e9b003842f94e993097e92
Saiful Islam <saifulanik@icloud.com>
2026-03-27 02:38:08 +0600

    character on jetski

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-Degen Island-2.4";
+const cacheName = "DefaultCompany-Degen Island-2.5";
 const contentToCache = [
     "Build/SpearMania-Airdrop.loader.js",
     "Build/SpearMania-Airdrop.framework.js",
