d9979538784d007169a7159832e69de50b60b0d6
Saiful Islam <saifulanik@icloud.com>
2026-03-27 21:19:25 +0600

    test

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-Degen Island-2.5";
+const cacheName = "DefaultCompany-Degen Island-2.6";
 const contentToCache = [
     "Build/SpearMania-Airdrop.loader.js",
     "Build/SpearMania-Airdrop.framework.js",
